7b0e20fbda3eb7628a10f28bd83c5ac9c379fc12
Eng-Benj <119880823+Eng-Benj@users.noreply.github.com>
2023-01-10 15:37:34 +0000

    Edit tempConversion.js to ensure tests are passed and the exercise is complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c =(f-32)*(5/9)
+  if (c % 1 != 0) {
+  c = c.toFixed(1)
+  }
+  return +c
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = (c*(9/5))+32
+  if (f % 1 != 0) {
+    f = f.toFixed(1)
+  }
+  return +f
 };
 
 // Do not edit below this line
