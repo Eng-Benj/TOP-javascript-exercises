8c552b890853b8db09d413ba79d242f34ceeaf87
Eng-Benj <119880823+Eng-Benj@users.noreply.github.com>
2023-01-10 14:19:51 +0000

    Edit repeatString.js to ensure tests are passed and the exercise is completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let concString = ""
+    for (let i = 1; i<=num; i++){
+        concString += string
+    }
+    if (num < 0) {
+        concString = "ERROR";
+    }
+    return concString
 };
 
 // Do not edit below this line
