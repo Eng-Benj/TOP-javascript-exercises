d2b6e63f2e2d031264f77cddac1aae69708d6a39
Eng-Benj <119880823+Eng-Benj@users.noreply.github.com>
2023-01-13 18:51:58 +0000

    Edit findTheOldest.js to ensure all tests are passed and the exercise is complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const thisYear = new Date().getFullYear()
+    const oldest = people.sort(function(a, b) {
+        if ("yearOfDeath" in a == false) {a.yearOfDeath = thisYear}
+        if ("yearOfDeath" in b == false) {b.yearOfDeath = thisYear}
+        const age1 = a.yearOfDeath - a.yearOfBirth;
+        const age2= b.yearOfDeath - b.yearOfBirth;
+        return age1 > age2 ? -1 : 1;
+      });
+      return people[0]
 };
 
 // Do not edit below this line
