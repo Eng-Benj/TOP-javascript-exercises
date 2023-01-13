2bb93b09225b24a719edf3ef958251b3e674ab6a
Eng-Benj <119880823+Eng-Benj@users.noreply.github.com>
2023-01-13 18:04:36 +0000

    Edit palindromes.js to ensure all tests are passed and the exercise is complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const initial = string.toLowerCase().split("");
+    const forwards = initial.filter(char => char !== " " && char !== "!" && char !== "." && char !== ",");
+    let backwards = forwards.slice();
+    backwards.reverse();
+    for (let i=0; i < forwards.length; i++) {
+        if (forwards[i] !== backwards[i]) {return false};
+    };
+    return true;
 };
 
+palindromes("A car, a man, a maraca.")
 // Do not edit below this line
 module.exports = palindromes;
