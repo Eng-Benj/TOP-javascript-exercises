42b4a746a9a7fde75089d1c70cec2f984cd9d89b
Eng-Benj <119880823+Eng-Benj@users.noreply.github.com>
2023-01-13 18:21:16 +0000

    Edit fibonacci.js to ensure all tests are passed and the exercise is complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    if (+member <0) {return "OOPS"}
+    let fibSeq = [1, 1]
+    for (let i = 2; i < +member; i++) {
+        fibSeq.push(fibSeq[i-2]+fibSeq[i-1])
+    }
+    return fibSeq[+member -1]
 };
 
+console.log(fibonacci(4))
 // Do not edit below this line
 module.exports = fibonacci;
