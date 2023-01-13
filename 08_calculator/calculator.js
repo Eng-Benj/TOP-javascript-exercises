131044258eefd94db85f95badffedf72aa825e60
Eng-Benj <119880823+Eng-Benj@users.noreply.github.com>
2023-01-13 16:35:50 +0000

    Edit calculator.js to ensure all tests are passed and the exercise is complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  if (numbers.length === 0) {return 0};
+	const numSum = numbers.reduce(number => {
+    let sum = 0;
+    for (let i=0; i < numbers.length; i++){
+      sum += Number(numbers[i]);
+    };
+    return sum;
+  });
+  return numSum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  const product = numbers.reduce(number => {
+    let parProd = 1;
+    for (let i=0; i < numbers.length; i++){
+      parProd *= Number(numbers[i])
+    };
+    return parProd;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1
+	for (let i = num; i > 0; i--) {
+    fact *=i
+  }
+  return fact
 };
 
 // Do not edit below this line
